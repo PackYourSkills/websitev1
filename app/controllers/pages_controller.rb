512e1ac6027e45a40ba23db13c7ee91328f81d3e
Maxime BDB <maxime@packyourskills.com>
2017-08-21 15:46:53 +0200

    november page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :NovemberIsTheNewCool]
 
   def home
+  end
+
+  def NovemberIsTheNewCool
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   scope '(:locale)', locale: /fr/ do #to use i18n | change /fr/ to /fr|es/ to add spanish
     root to: 'pages#home'
+    get 'NovemberIsTheNewCool', to: 'pages#NovemberIsTheNewCool'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
