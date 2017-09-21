2fbcd6b2ae049673b20083396e82c4292d7bec2b
Maxime BDB <maxime@packyourskills.com>
2017-09-21 11:10:16 +0200

    preprod

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ admin.save!
 
 fakepacker = User.new(
   email: 'packer@packyourskills.com',
-  password: '10max89',
+  password: 'iamapacker',
   role: 'packer',
   admin: false,
 )
@@ -26,7 +26,7 @@ fakepacker.save!
 
 packer = fakepacker.build_packer(
   first_name:'Packer',
-  last_name:'Admin',
+  last_name:'Test',
   sexe:'male',
   nationality:'French',
   story:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -34,7 +34,7 @@ packer = fakepacker.build_packer(
   value1:'Beer',
   value2:'Music',
   value3:'Mountain',
-  quote:'Blablabla',
+  quote:'Blablabla bla bla bla blaaablabla',
   quote_author:'myself',
   city:'Paris',
   country:'FR',
