a6ae21f88b3deb263dba7756df754bff3f2d6212
Maxime BDB <maxime@packyourskills.com>
2017-09-22 02:00:33 +0200

    test 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
   config.action_mailer.default_url_options = { host: 'packyourskills.com' }
 
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
