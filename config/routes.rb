665a4fa728908273d119ccafa2172ebea3644c6e
Maxime BDB <maxime@packyourskills.com>
2018-03-07 09:19:53 +0100

    route aviva

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   end
 
   get "/CallMaxime" => redirect("https://calendly.com/maximebdb/packyourskills")
+  get "/Aviva" => redirect("https://lafabrique-france.aviva.com/voting/projet/vue/30-1600")
 
   ## temporary #########
   get 'NovemberIsTheNewCool', to: 'missions#index', as: :novemberisthenewcool
