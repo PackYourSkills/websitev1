13a586caa481afdee44814e0304df0b9982363f5
Maxime BDB <maxime@packyourskills.com>
2018-08-27 21:47:34 +0200

    tentative 2 de blog

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get "/joinourteam" => redirect("https://medium.com/@maxime_bdb/pack-your-skills-rejoignez-laventure-65a7e22a407"), as: :joinus
 
   ## BLOG
-  get "/blog", to: redirect('https://www.packyourskills.com/blog/', status: 301)
+  get "/blog", to: redirect('www.packyourskills.com/blog/', status: 301)
   # get "/blog" => redirect("https://medium.com/pack-your-skills")
 
   ## temporary #########
