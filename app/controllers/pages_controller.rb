f98e4232507a61b0f40fe0c264192e15c4a4f3a9
Maxime BDB <maxime@packyourskills.com>
2018-10-04 17:58:32 +0200

    test bug double missions retour

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -60,8 +60,8 @@ class PagesController < ApplicationController
   def crew_room
     @crew = current_user.crew
     authorize @crew
-    @missions = @crew.missions
-    @connections = @missions.connections
+    @mission = @crew.missions.last
+    @connections = @mission.connections
     @all_online = @connections.select { |c| c.online? }
   end
 end
