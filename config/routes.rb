6b175a8ecb858032d3d8a5271fb7d216e4791747
Maxime BDB <maxime@packyourskills.com>
2019-01-16 23:27:20 +0100

    link home to recruitemne

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   end
 
   get "/CallMaxime" => redirect("https://calendly.com/maximebdb/packyourskills")
-  get "/joinourteam" => redirect("https://medium.com/@maxime_bdb/pack-your-skills-rejoignez-laventure-65a7e22a407"), as: :joinus
+  get "/joinourteam" => redirect("http://www.packyourskills.com/RecrutementJanvier2019.pdf"), as: :joinus
 
   ## BLOG
   get "/blog", to: redirect('http://blog.packyourskills.com/')
